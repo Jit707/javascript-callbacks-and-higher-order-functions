6ac7dac7592c2012ca4b47aff0df3e34bc572e34
Jit <jit.oung254343@gmail.com>
2024-08-31 21:23:57 +0700

    add finished assignment

--- ex-1.js
+++ ex-1.js
@@ -1,12 +1,24 @@
 // Exercise #1: For Each Function
 
-function forEach(array, operation) {
-  // Start coding here
+
+function forEach (array, operation){
+  let newArrays=[]
+  let num = 5000
+  for (let i=0 ; i<array.length; i++){
+    newArrays.push (operation(array[i],num))
+  }
+  return newArrays
+}
+ 
+function sum(a,b){
+  return a+b
 }
 
+
 const employeeSalaries = [20005, 40000, 32000, 14500, 344000];
-const newEmployeeSalaries = [];
+let newEmployeeSalaries = [];
 
 // Using `forEach` function here
+newEmployeeSalaries = forEach(employeeSalaries,sum)
 
 console.log(newEmployeeSalaries); // [25005, 45000, 37000, 19500, 349000]
--- ex-2.js
+++ ex-2.js
@@ -1,8 +1,28 @@
 //Exercise #2: At Least Five Function
 
 function atLeastFive(array, operation) {
-  // Start coding here
+  let result
+  for (let i=0 ; i<array.length; i++ ){
+    if (array[i] > [5]){
+      result = true
+    }else if ( array[i] < [5]){
+      result = false
+    }else{
+      result = "valid"
+    }
+  }
+ return result
 }
+function score(array){
+for (let i=0 ; i<array.length; i++ ){
+  if (array[i] > 70){
+  scoreresultmorethan70 = true
+  }else{
+    scoreresultmorethan70 = false
+  }}
+}
+
+
 
 const studentScoresRoom1 = [90, 40, 67, 80, 100, 15, 86, 12, 99, 67];
 const studentScoresRoom2 = [78, 98, 23, 15, 40, 12, 40, 67, 80, 100];
@@ -10,6 +30,6 @@ const studentScoresRoom3 = [67, 80, 100, 15, 12, 40, 67, 80, 100, 67];
 
 // Using `atLeastFive` function here
 
-let scoreRoom1Result;
-let scoreRoom2Result;
+let scoreRoom1Result 
+let scoreRoom2Result 
 let scoreRoom3Result;
